ee9125301fd8d312ae8cee0699753a68c693a924
Samuel <samueljonascali@gmail.com>
2025-08-05 12:48:36 -0300

    Create Empresas models for CSV archives

--- ibge_data_project/localities/models.py
+++ ibge_data_project/localities/models.py
@@ -31,3 +31,18 @@ class District(models.Model):
     def __str__(self):
         return self.name
 
+class Empresa(models.Model):
+    """
+    Modelo para representar os dados das empresas.
+    Baseado no layout do arquivo "Empresas" do CNPJ.
+    """
+    cnpj_basico = models.CharField(max_length=8, unique=True, primary_key=True)
+    razao_social = models.CharField(max_length=200, null=True, blank=True)
+    natureza_juridica = models.CharField(max_length=4, null=True, blank=True)
+    qualificacao_responsavel = models.CharField(max_length=2, null=True, blank=True)
+    capital_social = models.CharField(max_length=20, null=True, blank=True)
+    porte_empresa = models.CharField(max_length=2, null=True, blank=True)
+    ente_federativo_responsavel = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.razao_social} ({self.cnpj_basico})"
